fec998737c5c1fa7603c7318792d8d700d3da25e
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-23 18:33:14 +0200

    Add verify wall

--- src/Wrapper/Router.tsx
+++ src/Wrapper/Router.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import assert from 'assert';
 import {connect} from 'react-redux';
 import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
 import NavbarComponent from '../components/navbar';
 import LoginPageComponent from '../pages/LoginPage';
-import {ReduxState} from '../utilities/types';
+import {ReduxState, Account} from '../utilities/types';
 import RegisterPageComponent from '../pages/RegisterPage';
 import FormPageWrapperComponent from '../pages/FormPageWrapper';
 
@@ -21,8 +22,15 @@ function DashboardWrapperComponent(props: DashboardWrapperComponentProps) {
 interface RouterComponentProps {
     loggingIn: boolean;
     loggedIn: boolean;
+    account: undefined | Account;
 }
 function RouterComponent(props: RouterComponentProps) {
+    let verifyWall = false;
+    if (props.loggedIn) {
+        assert(props.account !== undefined);
+        verifyWall = !props.account.email_verified;
+    }
+
     return (
         <BrowserRouter>
             <Route>
@@ -30,23 +38,31 @@ function RouterComponent(props: RouterComponentProps) {
                     <Route path='(/configurations|/results|/account)'>
                         {props.loggingIn && <h3>Loading ...</h3>}
                         {!props.loggingIn && props.loggedIn && (
-                            <DashboardWrapperComponent>
-                                <Switch>
-                                    <Route exact path='/configurations'>
-                                        <div>Configurations</div>
-                                    </Route>
-                                    <Route exact path='/results'>
-                                        <div>Results</div>
-                                    </Route>
-                                    <Route exact path='/account'>
-                                        <div>Account</div>
-                                    </Route>
-                                </Switch>
-                            </DashboardWrapperComponent>
+                            <React.Fragment>
+                                {verifyWall && <Redirect to='/verify' />}
+                                {!verifyWall && (
+                                    <DashboardWrapperComponent>
+                                        <Switch>
+                                            <Route exact path='/configurations'>
+                                                <div>Configurations</div>
+                                            </Route>
+                                            <Route exact path='/results'>
+                                                <div>Results</div>
+                                            </Route>
+                                            <Route exact path='/account'>
+                                                <div>Account</div>
+                                            </Route>
+                                        </Switch>
+                                    </DashboardWrapperComponent>
+                                )}
+                            </React.Fragment>
                         )}
                         {!props.loggingIn && !props.loggedIn && (
                             <Redirect to='/login' />
                         )}
+                    </Route>
+                    <Route exact path='/verify'>
+                        Please verify your account first!
                     </Route>
                     <Route path='(/login|/register)'>
                         {props.loggingIn && <h3>Loading ...</h3>}
@@ -78,6 +94,7 @@ function RouterComponent(props: RouterComponentProps) {
 const mapStateToProps = (state: ReduxState) => ({
     loggingIn: state.loggingIn,
     loggedIn: state.loggedIn,
+    account: state.account,
 });
 const mapDispatchToProps = (dispatch: any) => ({});
 export default connect(mapStateToProps, mapDispatchToProps)(RouterComponent);
