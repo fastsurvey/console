fe7750ae649c73f23141cc6617fec979384d602c
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-10-28 20:34:41 +0100

    Add survey config types

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -60,3 +60,76 @@ export interface OpenModalAction {
 export interface CloseModalAction {
     type: 'CLOSE_MODAL';
 }
+
+// --------------------------------------------------------
+
+export interface SurveyConfig {
+    admin_name: 'fastsurvey';
+    description: '';
+    end: 2000000000;
+    mode: 0;
+    start: 1000000000;
+    survey_name: 'radio';
+    title: 'Radio Test';
+    fields: SurveyField[];
+}
+
+export type SurveyField =
+    | EmailField
+    | OptionField
+    | RadioField
+    | SelectionField
+    | TextField;
+
+export interface EmailField {
+    type: 'Email';
+    title: string;
+    description: string;
+    regex: string;
+    hint: string;
+}
+
+export interface OptionField {
+    type: 'Option';
+    title: string;
+    description: string;
+    mandatory: boolean;
+}
+
+export interface RadioField {
+    type: 'Radio';
+    title: string;
+    description: string;
+    fields: RadioFieldOption[];
+}
+
+export interface RadioFieldOption {
+    type: 'Option';
+    title: string;
+    description: string;
+    mandatory: false;
+}
+
+export interface SelectionField {
+    type: 'Selection';
+    title: string;
+    description: string;
+    min_select: number;
+    max_select: number;
+    fields: SelectionFieldOption[];
+}
+
+export interface SelectionFieldOption {
+    type: 'Option';
+    title: string;
+    description: string;
+    mandatory: false;
+}
+
+export interface TextField {
+    type: 'Text';
+    title: string;
+    description: string;
+    min_chars: number;
+    max_chars: number;
+}
