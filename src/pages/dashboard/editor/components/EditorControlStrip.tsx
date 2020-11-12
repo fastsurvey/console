b6a639e79aff068b784c60d245162e1e72d89ad9
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-11-12 10:43:48 +0100

    Add publish button to control strip

--- src/pages/dashboard/editor/components/EditorControlStrip.tsx
+++ src/pages/dashboard/editor/components/EditorControlStrip.tsx
@@ -7,6 +7,7 @@ interface ControlStripButtonProps {
     icon: React.ReactNode;
     first?: boolean;
     last?: boolean;
+    disabled?: boolean;
 }
 
 function ControlStripButton(props: ControlStripButtonProps) {
@@ -14,7 +15,9 @@ function ControlStripButton(props: ControlStripButtonProps) {
         <div
             className={
                 'py-1 px-2 h-10 flex flex-row items-center justify-center ' +
-                'cursor-pointer bg-white hover:text-blue-600 ' +
+                (props.disabled
+                    ? 'cursor-not-allowed bg-gray-200 '
+                    : 'cursor-pointer bg-white hover:text-blue-600 ') +
                 (props.first ? 'rounded-l ' : '') +
                 (props.last ? 'rounded-r ' : '') +
                 (!props.first ? 'border-l ' : '') +
@@ -33,13 +36,22 @@ interface EditorControlStripProps {
 function EditorControlStrip(props: EditorControlStripProps) {
     return (
         <div className={'fixed h-10 flex flex-row items-center shadow rounded'}>
-            <ControlStripButton first label='Start Now' icon={ICONS.play} />
-            <ControlStripButton label='End Now' icon={ICONS.stop} />
             <ControlStripButton
-                last
-                label='Save Configuration'
-                icon={ICONS.save}
+                disabled={props.config.draft}
+                first
+                label='Start Now'
+                icon={ICONS.play}
             />
+            <ControlStripButton
+                disabled={props.config.draft}
+                label='End Now'
+                icon={ICONS.stop}
+            />
+            <ControlStripButton
+                label={props.config.draft ? 'Publish' : 'Draft'}
+                icon={props.config.draft ? ICONS.open_in_browser : ICONS.create}
+            />
+            <ControlStripButton last label='Save' icon={ICONS.save} />
         </div>
     );
 }
