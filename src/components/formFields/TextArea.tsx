807216ed6bc9ff2e074fc0f27549067a618576b8
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-11-12 15:09:02 +0100

    Add TextArea component for survey description

---
+++ src/components/formFields/TextArea.tsx
@@ -0,0 +1,95 @@
+import React, {useState} from 'react';
+
+interface TextAreaProps {
+    value: string;
+    onChange(newValue: string): void;
+    placeholder?: string;
+    className?: string;
+    wrapperClassName?: string;
+    charLimits?: {min: number; max: number};
+    autoComplete?: string;
+    onEnter?(): void;
+    flat?: boolean;
+    rows?: number;
+}
+
+const TextArea = React.forwardRef((props: TextAreaProps, ref: any) => {
+    const placeholder =
+        props.placeholder !== undefined ? props.placeholder : '';
+    const autoComplete =
+        props.autoComplete !== undefined ? props.autoComplete : '';
+
+    const [focused, setFocus] = useState(false);
+
+    let hintFulfilled: boolean = true;
+    if (props.charLimits !== undefined) {
+        hintFulfilled =
+            props.charLimits.min <= props.value.length &&
+            props.value.length <= props.charLimits.max;
+    }
+
+    function handleKeyDown(e: any) {
+        if (e.key === 'Enter') {
+            if (props.onEnter !== undefined) {
+                props.onEnter();
+            }
+        } else if (e.key === 'Escape') {
+            e.target.blur();
+        }
+    }
+
+    return (
+        <div
+            className={
+                'relative ' +
+                (props.wrapperClassName ? props.wrapperClassName : '')
+            }
+        >
+            <textarea
+                rows={4}
+                ref={ref}
+                onKeyDown={handleKeyDown}
+                placeholder={placeholder}
+                value={props.value}
+                onChange={(e) => props.onChange(e.target.value)}
+                onFocus={() => setFocus(true)}
+                onBlur={() => setFocus(false)}
+                className={
+                    'font-weight-500 text-lg md:no-selection ' +
+                    'border-0 rounded w-full ' +
+                    'py-2 pl-3 pr-10 md:pr-12 text-gray-700 ' +
+                    (props.flat
+                        ? 'shadow-outline-gray bg-white transition duration-150 '
+                        : 'shadow ') +
+                    'focus:outline-none focus:shadow-outline ' +
+                    (props.className ? props.className : '')
+                }
+                autoComplete={autoComplete}
+            />
+            {props.charLimits && (
+                <div
+                    className={
+                        'absolute top-0 right-0 w-2 h-2 mt-5 mr-2 rounded-full ' +
+                        (hintFulfilled ? 'bg-green-500' : 'bg-magenta')
+                    }
+                />
+            )}
+            {props.charLimits && (
+                <div
+                    className={
+                        'relative z-50 w-full px-1 leading-6 ' +
+                        'overflow-hidden font-weight-600 ' +
+                        (hintFulfilled ? 'text-green-500' : 'text-magenta') +
+                        ' transition-all duration-300 ' +
+                        (focused ? 'h-8 py-1 mb-2' : 'h-0 py-0 mb-0')
+                    }
+                >
+                    {props.charLimits.min !== 0 ? 'Not empty, ' : ''}
+                    {props.value.length}/{props.charLimits.max} characters
+                </div>
+            )}
+        </div>
+    );
+});
+
+export default TextArea;
--- src/pages/dashboard/editor/components/GeneralConfig.tsx
+++ src/pages/dashboard/editor/components/GeneralConfig.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TextArea from '../../../../components/formFields/TextArea';
 import TextInput from '../../../../components/formFields/TextInput';
 import {SurveyConfig} from '../../../../utilities/types';
 
@@ -9,8 +10,8 @@ interface GeneralConfigProps {
 function GeneralConfig(props: GeneralConfigProps) {
     return (
         <div className='flex flex-col w-full mt-24'>
-            <div className='flex flex-row'>
-                <div className='flex flex-row items-start w-1/2 h-12 pr-4'>
+            <div className='flex flex-row mb-4'>
+                <div className='flex flex-row items-start w-1/2 pr-4'>
                     <div className='h-12 mr-3 text-xl font-weight-600 leading-12'>
                         Title:
                     </div>
@@ -19,6 +20,15 @@ function GeneralConfig(props: GeneralConfigProps) {
                         onChange={() => {}}
                         flat
                         wrapperClassName='self-stretch flex-grow'
+                        placeholder='The title of your survey'
+                        hint={{
+                            text:
+                                'Not empty, max. 120 characters ' +
+                                `(${120 - props.config.title.length} left)`,
+                            fulfilled:
+                                1 <= props.config.title.length &&
+                                props.config.title.length <= 120,
+                        }}
                     />
                 </div>
                 <div className='flex flex-row items-start w-1/2 pl-4'>
@@ -32,14 +42,33 @@ function GeneralConfig(props: GeneralConfigProps) {
                         wrapperClassName='self-stretch flex-grow'
                         placeholder='URL conform identifier'
                         hint={{
-                            text: "Only characters, numbers and '-' or '_'",
+                            text:
+                                'URL-conform, 3-120 ' +
+                                `characters (${
+                                    120 - props.config.title.length
+                                } left)`,
                             fulfilled:
                                 props.config.survey_name.match(
                                     /^[a-zA-Z0-9-_]*$/,
-                                ) !== null,
+                                ) !== null &&
+                                3 <= props.config.title.length &&
+                                props.config.title.length <= 120,
                         }}
                     />
                 </div>
+            </div>
+            <div className='flex flex-row items-start w-full h-32'>
+                <div className='h-12 mr-3 text-xl font-weight-600 leading-12'>
+                    Description:
+                </div>
+                <TextArea
+                    value={props.config.title}
+                    onChange={() => {}}
+                    flat
+                    charLimits={{min: 0, max: 500}}
+                    className='leading-8'
+                    wrapperClassName='self-stretch flex-grow'
+                />
             </div>
         </div>
     );
