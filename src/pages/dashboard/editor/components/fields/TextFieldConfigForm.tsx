0fe1d0c5a8d11e82fd8f4d04a3fb0d6260b1342e
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-11-13 17:17:15 +0100

    Add settings section to TextFieldConfigForm

--- src/pages/dashboard/editor/components/fields/TextFieldConfigForm.tsx
+++ src/pages/dashboard/editor/components/fields/TextFieldConfigForm.tsx
@@ -16,11 +16,14 @@ function TextFieldConfigForm(props: TextFieldConfigFormProps) {
                 'w-full bg-gray-100 shadow-outline-gray rounded flex flex-col'
             }
         >
-            <div className={'w-full border-b-4 border-gray-300 flex flex-row'}>
+            <div
+                className={'w-full border-gray-300 flex flex-row'}
+                style={{borderBottomWidth: '2px'}}
+            >
                 <div className={'flex flex-row'}>
                     <div
                         className={
-                            'bg-gray-300 rounded-tl rounded-br h-10 leading-10 font-weight-600 text-xl flex flex-row'
+                            'bg-gray-300 rounded-tl rounded-br h-10 leading-10 font-weight-700 text-xl flex flex-row'
                         }
                     >
                         <div className='w-10 h-10 p-2 cursor-move'>
@@ -73,7 +76,55 @@ function TextFieldConfigForm(props: TextFieldConfigFormProps) {
                     </div>
                 </div>
             </div>
-            <div className={'p-2 w-full'}>hello</div>
+            <div className={'w-full flex flex-row'}>
+                <div className={'flex flex-row'}>
+                    <div
+                        className={
+                            'bg-gray-300 rounded-br h-10 leading-10 font-weight-500 text-xl flex flex-row'
+                        }
+                    >
+                        <div className='w-10 h-10 p-2 cursor-move'>
+                            {ICONS.tune}
+                        </div>
+                        <div className='pr-4'>Settings</div>
+                    </div>
+                </div>
+                <div className={'flex flex-row p-2 ml-2 '}>
+                    <div className='flex flex-row items-start mr-8'>
+                        <div className='h-12 mr-3 text-xl font-weight-600 leading-12'>
+                            Min. Characters:
+                        </div>
+                        <TextInput
+                            value={props.fieldConfig.min_chars.toString()}
+                            onChange={() => {}}
+                            flat
+                            wrapperClassName='w-28'
+                            hint={{
+                                text: '<= max char.',
+                                fulfilled:
+                                    0 <= props.fieldConfig.min_chars &&
+                                    props.fieldConfig.min_chars <=
+                                        props.fieldConfig.max_chars,
+                            }}
+                        />
+                    </div>
+                    <div className='flex flex-row items-start mr-8'>
+                        <div className='h-12 mr-3 text-xl font-weight-600 leading-12'>
+                            Max. Characters:
+                        </div>
+                        <TextInput
+                            value={props.fieldConfig.max_chars.toString()}
+                            onChange={() => {}}
+                            flat
+                            wrapperClassName='w-28'
+                            hint={{
+                                text: '<= 2000',
+                                fulfilled: props.fieldConfig.max_chars <= 2000,
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
