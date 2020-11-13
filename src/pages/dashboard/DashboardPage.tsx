3ddc1643b492853200052f81dd60f437909789aa
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-11-14 00:45:02 +0100

    Fix #13

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import MobileNavbar from '../../components/navbar/MobileNavbar';
 import Navbar from '../../components/navbar/RegularNavbar';
-import {ReduxState} from '../../utilities/types';
+import {addConfigsAction} from '../../utilities/reduxActions';
+import {fetchSurveys} from '../../utilities/surveyCommunication';
+import {ReduxState, SurveyConfig, OAuth2Token} from '../../utilities/types';
 import './DashboardPage.scss';
 
 interface DashBoardPageProps {
     children: React.ReactNode;
     modalOpen: boolean;
+    loggedIn: boolean;
+    oauth2_token: OAuth2Token | undefined;
+    addConfigs(configs: SurveyConfig[]): void;
 }
 function DashBoardPage(props: DashBoardPageProps) {
+    useEffect(() => {
+        async function fetch(oauth2_token: OAuth2Token) {
+            await fetchSurveys(oauth2_token, (configs: SurveyConfig[]) => {
+                configs.forEach((config, index) => {
+                    config.local_id = index;
+                });
+                props.addConfigs(configs);
+            });
+        }
+        if (props.loggedIn && props.oauth2_token !== undefined) {
+            fetch(props.oauth2_token);
+        }
+    }, [props.loggedIn, props.oauth2_token]);
+
     return (
         <React.Fragment>
             <header>
@@ -42,6 +61,11 @@ function DashBoardPage(props: DashBoardPageProps) {
 
 const mapStateToProps = (state: ReduxState) => ({
     modalOpen: state.modalOpen,
+    loggedIn: state.loggedIn,
+    oauth2_token: state.oauth2_token,
 });
-const mapDispatchToProps = (dispatch: any) => ({});
+const mapDispatchToProps = (dispatch: any) => ({
+    addConfigs: (configs: SurveyConfig[]) =>
+        dispatch(addConfigsAction(configs)),
+});
 export default connect(mapStateToProps, mapDispatchToProps)(DashBoardPage);
