b35de39030d6618f65766b026bd8b711fe6e647b
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-11-18 14:37:49 +0100

    Further improve SelectionSettings

--- src/pages/configuration/editor/fields/selection-settings/visual-selection-settings.tsx
+++ src/pages/configuration/editor/fields/selection-settings/visual-selection-settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {configTypes, hints} from 'utilities';
-import {TextInput} from 'components';
+import {configTypes, formatters, hints} from 'utilities';
+import {TextInput, VisualTextInputRow} from 'components';
 import FieldOptionsList from '../field-options-list/field-options-list';
 
 interface VisualSelectionSettingsProps {
@@ -9,52 +9,34 @@ interface VisualSelectionSettingsProps {
     disabled: boolean;
 }
 function VisualSelectionSettings(props: VisualSelectionSettingsProps) {
-    const commonSelectionProps = {
+    const commonProps = {
+        config: props.fieldConfig,
         disabled: props.disabled,
-        flat: true,
+        updateConfig: props.updateFieldConfig,
         wrapperClassName: 'w-28',
     };
+
     return (
         <div className='flex flex-col'>
-            <div className='flex flex-row mb-2'>
-                <div className='flex flex-row items-start mr-8'>
-                    <div className='h-12 ml-2 mr-3 text-xl font-weight-600 leading-12'>
-                        Min. Selection:
-                    </div>
-                    <TextInput
-                        {...commonSelectionProps}
-                        value={props.fieldConfig.min_select.toString()}
-                        onChange={(newValue: string) =>
-                            props.updateFieldConfig({
-                                ...props.fieldConfig,
-                                min_select:
-                                    newValue.length > 0
-                                        ? parseInt(newValue)
-                                        : 0,
-                            })
-                        }
-                        hint={hints.minSelect(props.fieldConfig)}
-                    />
-                </div>
-                <div className='flex flex-row items-start mr-8'>
-                    <div className='h-12 mr-3 text-xl font-weight-600 leading-12'>
-                        Max. Selection:
-                    </div>
-                    <TextInput
-                        {...commonSelectionProps}
-                        value={props.fieldConfig.max_select.toString()}
-                        onChange={(newValue: string) =>
-                            props.updateFieldConfig({
-                                ...props.fieldConfig,
-                                max_select:
-                                    newValue.length > 0
-                                        ? parseInt(newValue)
-                                        : 0,
-                            })
-                        }
-                        hint={hints.maxSelect(props.fieldConfig)}
-                    />
-                </div>
+            <div className='flex flex-row mb-2 ml-2 gap-x-8'>
+                <VisualTextInputRow
+                    {...commonProps}
+                    label='Min. Selection'
+                    value={props.fieldConfig.min_select.toString()}
+                    onChange={(newValue: string) => ({
+                        min_select: formatters.atoi(newValue),
+                    })}
+                    hint={hints.minSelect(props.fieldConfig)}
+                />
+                <VisualTextInputRow
+                    {...commonProps}
+                    label='Max. Selection'
+                    value={props.fieldConfig.max_select.toString()}
+                    onChange={(newValue: string) => ({
+                        max_select: formatters.atoi(newValue),
+                    })}
+                    hint={hints.maxSelect(props.fieldConfig)}
+                />
             </div>
             <FieldOptionsList
                 fieldConfig={props.fieldConfig}
