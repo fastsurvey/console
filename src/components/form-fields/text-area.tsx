a0c6d0dc4e107ab2ccd7db0b4889560b8dc9d982
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-12-28 16:46:59 +0100

    Reduce TextArea complexity (yagni)

--- src/components/form-fields/text-area.tsx
+++ src/components/form-fields/text-area.tsx
@@ -4,20 +4,11 @@ interface Props {
     value: string;
     onChange(newValue: string): void;
     placeholder?: string;
-    className?: string;
-    wrapperClassName?: string;
     charLimits?: {min: number; max: number};
-    autoComplete?: string;
-    onEnter?(): void;
     rows?: number;
     disabled?: boolean;
 }
 const TextArea = React.forwardRef((props: Props, ref: any) => {
-    const placeholder =
-        props.placeholder !== undefined ? props.placeholder : '';
-    const autoComplete =
-        props.autoComplete !== undefined ? props.autoComplete : '';
-
     const [focused, setFocus] = useState(false);
 
     let hintFulfilled: boolean = true;
@@ -28,44 +19,33 @@ const TextArea = React.forwardRef((props: Props, ref: any) => {
     }
 
     function handleKeyDown(e: any) {
-        if (e.key === 'Enter') {
-            if (props.onEnter !== undefined) {
-                props.onEnter();
-            }
-        } else if (e.key === 'Escape') {
+        if (e.key === 'Escape') {
             e.target.blur();
         }
     }
 
     return (
-        <div
-            className={
-                'relative ' +
-                (props.wrapperClassName ? props.wrapperClassName : '')
-            }
-        >
+        <div className='relative w-full'>
             <textarea
                 disabled={props.disabled === true}
                 rows={props.rows ? props.rows : 2}
                 ref={ref}
                 onKeyDown={handleKeyDown}
-                placeholder={placeholder}
+                placeholder={props.placeholder}
                 value={props.value}
                 onChange={(e) => props.onChange(e.target.value)}
                 onFocus={() => setFocus(true)}
                 onBlur={() => setFocus(false)}
                 className={
-                    'font-weight-500 text-lg md:no-selection ' +
+                    'font-weight-500 text-lg leading-8 no-selection ' +
                     'border-0 rounded w-full min-h-12 max-h-128 ' +
                     'py-2 pl-3 pr-12 text-gray-800 leading-8 ' +
                     'transition-all duration-100 bg-gray-100 ' +
                     'outline-none focus:ring ring-blue-300 ' +
-                    (props.className ? props.className : '') +
                     (props.disabled
                         ? ' bg-gray-200 cursor-not-allowed '
                         : ' bg-white ')
                 }
-                autoComplete={autoComplete}
             />
             {props.charLimits && (
                 <div
