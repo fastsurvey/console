0e5b6ea5e2989279d2881281efde8e4a044f29a2
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-12-30 17:44:50 +0100

    Add open and closing of the popup with modal

--- src/pages/configuration/editor/add-field-panel/add-field-panel.tsx
+++ src/pages/configuration/editor/add-field-panel/add-field-panel.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import icons from 'assets/icons/icons';
 import AddFieldButton from './add-field-button';
 import AddFieldPopup from './add-field-popup';
 
 interface Props {}
 function AddFieldPanel(props: Props) {
+    const [popupOpen, setPopupOpen] = useState(false);
+
     return (
-        <div
-            className={
-                'relative w-full group gap-x-2 ' +
-                'h-8 hover:h-10 hover:my-2 ' +
-                'transition-size duration-200 ' +
-                'flex flex-row items-center justify-center '
-            }
-        >
-            <AddFieldButton label='add field' icon={icons.addBox} />
-            <AddFieldPopup />
-            <AddFieldButton label='paste field' icon={icons.widgets} leftIcon />
-        </div>
+        <>
+            <div
+                className={
+                    'fixed top-0 left-0 z-40 w-full h-full bg-gray-800 transition-opacity duration-300 ' +
+                    (popupOpen ? 'opacity-40' : 'opacity-0 pointer-events-none')
+                }
+            />
+
+            <div
+                className={
+                    'relative w-full group gap-x-2 ' +
+                    'h-8 hover:h-10 hover:my-2 ' +
+                    'transition-size duration-200 ' +
+                    'flex flex-row items-center justify-center '
+                }
+                onMouseLeave={() => setPopupOpen(false)}
+            >
+                <AddFieldButton
+                    label='add field'
+                    icon={icons.addBox}
+                    onClick={() => setPopupOpen(true)}
+                />
+                <AddFieldPopup open={popupOpen} />
+            </div>
+        </>
     );
 }
 
--- src/pages/configuration/editor/add-field-panel/add-field-popup.tsx
+++ src/pages/configuration/editor/add-field-panel/add-field-popup.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import {colors} from 'utilities';
 import {configTypes} from 'utilities';
 
-interface Props {}
+interface Props {
+    open: boolean;
+}
 function AddFieldPopup(props: Props) {
     const fields: configTypes.FieldType[] = [
         'Email',
@@ -15,7 +17,8 @@ function AddFieldPopup(props: Props) {
     return (
         <div
             className={
-                'absolute z-50 top-0 mt-0 group-hover:mt-2 left-1/2 w-40 px-1 rounded shadow-md transform -translate-x-1/2 transition-size duration-200 bg-white cursor-pointer'
+                'absolute z-50 top-0 mt-0 left-1/2 w-40 px-1 rounded shadow-md transform -translate-x-1/2 transition-size duration-200 bg-white cursor-pointer ' +
+                (props.open ? 'scale-y-100' : 'scale-y-0')
             }
         >
             {fields.map((fieldType: configTypes.FieldType, i: number) => (
@@ -23,6 +26,7 @@ function AddFieldPopup(props: Props) {
                     className={
                         'flex flex-row items-start justify-start ' +
                         'my-1 text-lg leading-10 font-weight-600 rounded ' +
+                        'opacity-70 hover:opacity-100 ' +
                         colors.colorToClasses(
                             colors.fieldTypeToColor(fieldType),
                         )
