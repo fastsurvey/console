711070d499e8097d83ea3dbecf5543c3dbe35b60
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-05-03 14:19:17 +0200

    Use remote backend

--- src/utilities/backend/http-clients.ts
+++ src/utilities/backend/http-clients.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {types} from 'types';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://backend.dev.fastsurvey.io';
 
 export function httpPost(
     url: string,
