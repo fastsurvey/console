9cdb1acb3d4bf4bace8c512375303b0a02d0dc9e
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-06-06 01:52:04 +0200

    Improve package.json and use local backend

--- src/utilities/backend/http-clients.ts
+++ src/utilities/backend/http-clients.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {types} from 'types';
 
-const API_URL = 'https://backend.dev.fastsurvey.io';
+const API_URL = 'http://localhost:8000';
 
 export function httpPost(
     url: string,
