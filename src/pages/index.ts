fcf3f72393f30a534bdd47786ff5d2916d4e86c6
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-06-11 18:42:00 +0200

    Remove verify-wall and form-page components

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,2 @@
-export {default as FormPage} from './pages-types/form-page';
 export {default as DashboardPage} from './pages-types/dashboard-page';
 export {default as NotFoundPage} from './not-found-page';
