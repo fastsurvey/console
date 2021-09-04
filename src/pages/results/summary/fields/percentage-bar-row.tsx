c2534f6bd4a41d9fc783148ad0f955cc3920faa8
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-09-04 20:51:57 +0200

    Use new colorbar design in option summary

--- src/pages/results/summary/fields/percentage-bar-row.tsx
+++ src/pages/results/summary/fields/percentage-bar-row.tsx
@@ -4,13 +4,30 @@ export default function PercentageBarRow(props: {
     title: string;
     count: number;
     total: number;
+    variant?: 'green' | 'red';
 }) {
     const {title, count, total} = props;
+    let barColor: string;
+    let textColor: string;
+    switch (props.variant) {
+        case 'green':
+            barColor = 'bg-green-400';
+            textColor = 'text-green-900';
+            break;
+        case 'red':
+            barColor = 'bg-red-400';
+            textColor = 'text-red-900';
+            break;
+        default:
+            barColor = 'bg-blue-400';
+            textColor = 'text-blue-900';
+            break;
+    }
     return (
         <div className='w-full space-y-[0.1875rem] flex-col-left first:pt-2'>
             <div className='w-full h-2 overflow-hidden rounded-sm flex-row-center'>
                 <div
-                    className='h-full bg-blue-400'
+                    className={`h-full ${barColor}`}
                     style={{
                         width: `${(count / total) * 100 + 0}%`,
                     }}
@@ -22,7 +39,9 @@ export default function PercentageBarRow(props: {
                     }}
                 />
             </div>
-            <div className='text-sm leading-tight text-blue-900 font-weight-700'>
+            <div
+                className={`text-sm leading-tight font-weight-700 ${textColor}`}
+            >
                 {title}:{' '}
                 <span className='font-weight-500'>
                     {count} vote
--- src/pages/results/summary/fields/summaries/option-summary.tsx
+++ src/pages/results/summary/fields/summaries/option-summary.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {types} from '@types';
+import PercentageBarRow from '../percentage-bar-row';
 
 export default function OptionSummary(props: {
     fieldConfig: types.SurveyField;
@@ -8,23 +9,19 @@ export default function OptionSummary(props: {
 }) {
     const {fieldResults, fieldConfig, count} = props;
     return (
-        <div className='w-full space-x-2 flex-row-left'>
-            <div className='text-green-700'>Yes ({fieldResults})</div>
-            <div className='flex-grow h-2 max-w-full overflow-hidden rounded-sm flex-row-center'>
-                <div
-                    className='h-full bg-green-200'
-                    style={{
-                        width: `${(fieldResults / count) * 100 + 0}%`,
-                    }}
-                />
-                <div
-                    className='h-full bg-red-200'
-                    style={{
-                        width: `${((count - fieldResults) / count) * 100 + 0}%`,
-                    }}
-                />
-            </div>
-            <div className='text-red-700'>No ({count - fieldResults})</div>
-        </div>
+        <>
+            <PercentageBarRow
+                total={count}
+                count={fieldResults}
+                title={'Yes'}
+                variant='green'
+            />
+            <PercentageBarRow
+                total={count}
+                count={count - fieldResults}
+                title={'No'}
+                variant='red'
+            />
+        </>
     );
 }
