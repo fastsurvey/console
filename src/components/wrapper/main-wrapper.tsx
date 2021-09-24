585fe697eccd5fb85209dc8951a65c263d9d8625
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-09-24 11:08:16 +0200

    Close #90

--- src/components/wrapper/main-wrapper.tsx
+++ src/components/wrapper/main-wrapper.tsx
@@ -7,11 +7,23 @@ function MainWrapper(props: {children: React.ReactNode; className?: string}) {
             <header className='z-40'>
                 <Logo />
             </header>
-            <main
-                className={`w-screen px-2 min-h-screen centering-col bg-gray-100 ${props.className}`}
-            >
-                {props.children}
-            </main>
+            <div className='relative'>
+                <main
+                    className={`w-screen px-2 min-h-screen centering-col bg-gray-100 ${props.className}`}
+                >
+                    {props.children}
+                </main>
+                <footer className='absolute bottom-0 left-0 z-0 w-full flex-row-center'>
+                    <div className='py-1 text-sm text-gray-400 font-weight-500'>
+                        {import.meta.env.MODE === 'development' && (
+                            <>development</>
+                        )}
+                        {import.meta.env.MODE === 'production' && (
+                            <>version {import.meta.env.VITE_COMMIT_SHA}</>
+                        )}
+                    </div>
+                </footer>
+            </div>
         </React.Fragment>
     );
 }
