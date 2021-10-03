340bfbe8e9277394b1d06c90295f04cfded88099
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-10-03 19:28:25 +0200

    Fix dev-deploy script

--- scripts/deploy-development.sh
+++ scripts/deploy-development.sh
@@ -4,9 +4,9 @@ export VITE_COMMIT_SHA="$(git rev-parse --short --verify HEAD)"
 export VITE_ENV="development"
 yarn build
 
-docker build -t gcr.io/fastsurvey-infrastructure/frontend-dev .
-docker push gcr.io/fastsurvey-infrastructure/frontend-dev:latest
+docker build -t gcr.io/fastsurvey-infrastructure/console-dev .
+docker push gcr.io/fastsurvey-infrastructure/console-dev:latest
 
-gcloud run deploy frontend-dev \
-    --image=gcr.io/fastsurvey-infrastructure/frontend-dev:latest \
+gcloud run deploy console-dev \
+    --image=gcr.io/fastsurvey-infrastructure/console-dev:latest \
     --platform managed --tag "commit-$VITE_COMMIT_SHA"
