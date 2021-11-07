dafb5dd1180f7d09112e0354d97985f7d4796653
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-11-07 13:15:39 +0100

    Refactor cypress dir, use fixture for account data

---
+++ cypress/integration/auth-pages/account_page.e2e.spec.ts
@@ -0,0 +1,306 @@
+Cypress.Cookies.debug(true);
+
+function assertTabState(selectedTab: 'identification' | 'password') {
+    cy.get('@settingsSection')
+        .find('button')
+        .contains(
+            selectedTab === 'identification' ? 'Identification' : 'Password',
+        )
+        .should('have.length', 1)
+        .should('have.class', 'bg-blue-50');
+
+    cy.get('@settingsSection')
+        .find('button')
+        .contains(
+            selectedTab === 'identification' ? 'Password' : 'Identification',
+        )
+        .should('have.length', 1)
+        .should('not.have.class', 'bg-blue-50')
+        .should('have.class', 'text-gray-500');
+}
+
+it('account page looks as expected, navigation works', () => {
+    cy.fixture('account.json').then((json) => {
+        const {username: USERNAME, password: PASSWORD} = json;
+
+        cy.visit('/login');
+        cy.get('input').first().type(USERNAME);
+        cy.get('input').last().type(PASSWORD);
+        cy.get('button').contains('Login').click();
+        cy.url().should('eq', 'http://localhost:3000/configurations');
+
+        cy.get('button')
+            .contains('Account')
+            .should('have.length', 1)
+            .parents('button')
+            .should('not.be.disabled')
+            .click({force: true});
+
+        cy.url().should('eq', 'http://localhost:3000/account');
+
+        cy.get('h1').contains('Modify your Account').should('have.length', 1);
+        cy.get('h2:visible').should('have.length', 3);
+        cy.get('h2')
+            .contains('Account Settings')
+            .should('have.length', 1)
+            .parents('section')
+            .as('settingsSection');
+        cy.get('h2')
+            .contains('Delete your account forever')
+            .should('have.length', 1)
+            .parents('section')
+            .as('deleteSection');
+        cy.get('h2').contains('Payment Information').should('have.length', 1);
+
+        assertTabState('identification');
+        cy.get('@settingsSection').find('input').should('have.length', 2);
+
+        cy.get('@settingsSection').find('button').contains('Password').click();
+        assertTabState('password');
+        cy.get('@settingsSection').find('input').should('have.length', 1);
+
+        cy.get('@settingsSection')
+            .find('button')
+            .contains('Identification')
+            .click();
+        assertTabState('identification');
+    });
+});
+
+function assertUsernameState(
+    value: string,
+    cancelActive: boolean,
+    changeActive: boolean,
+) {
+    if (value.length > 0) {
+        cy.get('@usernameInput').should('have.value', value);
+    } else {
+        cy.get('@usernameInput').should('be.empty');
+    }
+    cy.get('@cancelUsername').should(
+        (cancelActive ? 'not.be' : 'be') + '.disabled',
+    );
+    cy.get('@changeUsername').should(
+        (changeActive ? 'not.be' : 'be') + '.disabled',
+    );
+}
+
+function assertValidation(props: {
+    parentSection: string;
+    valid: boolean;
+    contains: string;
+}) {
+    // TODO: Test the validation message text, not only the color
+    cy.get(props.parentSection)
+        .find('div')
+        .contains(props.contains)
+        .should('have.length', 1)
+        .parent()
+        .should('have.class', props.valid ? 'text-green-900' : 'text-red-900')
+        .find('svg')
+        .should('have.length', 1)
+        .parent()
+        .should(
+            'have.class',
+            props.valid ? 'icon-dark-green' : 'icon-dark-red',
+        );
+}
+
+function initSettingsSection() {
+    cy.get('h2:visible')
+        .should('have.length', 3)
+        .contains('Account Settings')
+        .parents('section')
+        .as('settingsSection');
+
+    cy.get('@settingsSection')
+        .find('input')
+        .should('have.length', 2)
+        .first()
+        .as('emailInput')
+        .should('have.attr', 'type')
+        .and('eq', 'text');
+
+    cy.get('@settingsSection')
+        .find('input')
+        .last()
+        .as('usernameInput')
+        .should('have.attr', 'type')
+        .and('eq', 'text');
+
+    cy.get('button')
+        .contains('cancel')
+        .should('have.length', 1)
+        .parents('button')
+        .as('cancelUsername');
+    cy.get('button')
+        .contains('change username')
+        .should('have.length', 1)
+        .parents('button')
+        .as('changeUsername');
+
+    /*
+     labels now present:
+        @settingsSection
+        @emailInput
+        @usernameInput
+        @cancelUsername
+        @changeUsername
+    */
+}
+
+it('username change works as expected', () => {
+    cy.fixture('account.json').then((json) => {
+        const {
+            username: USERNAME,
+            tmpUsername: TMP_USERNAME,
+            password: PASSWORD,
+        } = json;
+
+        cy.visit('/login');
+        cy.get('input').first().type(USERNAME);
+        cy.get('input').last().type(PASSWORD);
+        cy.get('button').contains('Login').click();
+        cy.url().should('eq', 'http://localhost:3000/configurations');
+
+        cy.get('button').contains('Account').click({force: true});
+        cy.url().should('eq', 'http://localhost:3000/account');
+
+        cy.get('h2')
+            .contains('Account Settings')
+            .parents('section')
+            .as('settingsSection');
+
+        assertTabState('identification');
+        initSettingsSection();
+
+        cy.get('@emailInput').should('be.disabled');
+        cy.get('@usernameInput').should('not.be.disabled');
+        assertUsernameState(USERNAME, false, false);
+
+        cy.get('@usernameInput').clear();
+        assertUsernameState('', true, false);
+        assertValidation({
+            parentSection: '@settingsSection',
+            contains: 'username too short (≥ 1 character)',
+            valid: false,
+        });
+
+        cy.get('@cancelUsername').click();
+        assertUsernameState(USERNAME, false, false);
+
+        cy.get('@usernameInput').clear().type(TMP_USERNAME);
+        assertUsernameState(TMP_USERNAME, true, true);
+
+        cy.get('@cancelUsername').click();
+        assertUsernameState(USERNAME, false, false);
+
+        cy.get('@usernameInput').clear().type(TMP_USERNAME);
+        cy.get('@changeUsername').click();
+
+        cy.get('h2')
+            .contains('Change username?')
+            .should('have.length', 1)
+            .parents('section')
+            .as('usernamePopup');
+
+        cy.get('@usernamePopup')
+            .find('div')
+            .contains(`fastsurvey.de/${USERNAME}/<survey-id>`)
+            .should('have.length', 1);
+        cy.get('@usernamePopup')
+            .find('div')
+            .contains(`fastsurvey.de/${TMP_USERNAME}/<survey-id>`)
+            .should('have.length', 1);
+
+        cy.get('@usernamePopup').find('button').should('have.length', 2);
+        cy.get('@usernamePopup')
+            .find('button')
+            .contains('confirm')
+            .should('have.length', 1);
+        cy.get('@usernamePopup')
+            .find('button')
+            .contains('cancel')
+            .should('have.length', 1)
+            .click();
+
+        cy.get('h2').contains('Change username?').should('have.length', 1);
+
+        assertUsernameState(USERNAME, false, false);
+
+        cy.get('@usernameInput').clear().type(TMP_USERNAME);
+        cy.get('@changeUsername').click();
+
+        cy.get('h2')
+            .contains('Change username?')
+            .parents('section')
+            .as('usernamePopup');
+
+        cy.get('@usernamePopup')
+            .find('button')
+            .contains('confirm')
+            .should('have.length', 1)
+            .click();
+
+        // wait until request is finished
+        cy.get('h2').contains('Change username?').should('not.be.visible');
+
+        assertUsernameState(TMP_USERNAME, false, false);
+
+        // refresh the page
+        cy.visit('/account');
+        cy.url().should('eq', 'http://localhost:3000/account');
+
+        initSettingsSection();
+        assertUsernameState(TMP_USERNAME, false, false);
+
+        // log out and log in again
+        cy.get('button')
+            .contains('Logout')
+            .should('have.length', 1)
+            .parents('button')
+            .should('not.be.disabled')
+            .click({force: true});
+        cy.url().should('eq', 'http://localhost:3000/login');
+
+        cy.get('input').first().type(TMP_USERNAME);
+        cy.get('input').last().type(PASSWORD);
+
+        cy.get('button').contains('Login').click();
+
+        cy.url().should('eq', 'http://localhost:3000/configurations');
+        cy.get('button').contains('Account').click({force: true});
+        cy.url().should('eq', 'http://localhost:3000/account');
+
+        initSettingsSection();
+        assertUsernameState(TMP_USERNAME, false, false);
+
+        cy.get('@usernameInput').clear().type(USERNAME);
+        assertUsernameState(USERNAME, true, true);
+        cy.get('@changeUsername').click();
+
+        cy.get('h2')
+            .contains('Change username?')
+            .should('have.length', 1)
+            .parents('section')
+            .as('usernamePopup');
+
+        cy.get('@usernamePopup')
+            .find('button')
+            .contains('confirm')
+            .should('have.length', 1)
+            .click();
+
+        // wait until request is finished
+        cy.get('h2').contains('Change username?').should('not.be.visible');
+
+        assertUsernameState(USERNAME, false, false);
+
+        // refresh the page
+        cy.visit('/account');
+        cy.url().should('eq', 'http://localhost:3000/account');
+
+        initSettingsSection();
+        assertUsernameState(USERNAME, false, false);
+    });
+});
---
+++ cypress/integration/auth-pages/login_page.e2e.spec.js
@@ -0,0 +1,131 @@
+describe('The Login Page', () => {
+    it('has working links to other auth-pages', () => {
+        cy.visit('/login');
+        cy.get('h1').should('have.length', 1).should('have.text', 'Login');
+        cy.get('a')
+            .contains("Don't have an account yet?")
+            .should('have.length', 1)
+            .click();
+        cy.url().should('eq', 'http://localhost:3000/register');
+        cy.visit('/login');
+        cy.get('a')
+            .contains('Forgot your password?')
+            .should('have.length', 1)
+            .click();
+        cy.url().should('eq', 'http://localhost:3000/forgot-password');
+    });
+
+    it('has expected inputs/labels and working password-visibility toggle', () => {
+        cy.visit('/login');
+        cy.get('input').should('have.length', 2);
+        cy.get('label').should('have.length', 2);
+        cy.get('input').first().type('abc').should('have.value', 'abc');
+        cy.get('input').last().type('abcde').should('have.value', 'abcde');
+
+        cy.get('input').first().should('have.attr', 'type').and('eq', 'text');
+        cy.get('input')
+            .last()
+            .should('have.attr', 'type')
+            .and('eq', 'password');
+        cy.get('svg').should('have.length', 1).click();
+        cy.get('input').last().should('have.attr', 'type').and('eq', 'text');
+        cy.get('svg').should('have.length', 1).click();
+        cy.get('input')
+            .last()
+            .should('have.attr', 'type')
+            .and('eq', 'password');
+
+        cy.get('button')
+            .contains('Login')
+            .should('have.length', 1)
+            .parents('button')
+            .should('be.disabled');
+
+        cy.get('input').last().type('fgh').should('have.value', 'abcdefgh');
+
+        cy.get('button')
+            .contains('Login')
+            .parents('button')
+            .should('not.be.disabled');
+    });
+
+    it('login and logout with blueberry test account works', () => {
+        cy.fixture('account.json').then((json) => {
+            const {username: USERNAME, password: PASSWORD} = json;
+            cy.visit('/login');
+            cy.get('input').should('have.length', 2);
+            cy.get('input').first().type(USERNAME);
+            cy.get('input').last().type(PASSWORD);
+
+            cy.get('button')
+                .contains('Login')
+                .parents('button')
+                .should('not.be.disabled')
+                .click();
+
+            cy.url().should('eq', 'http://localhost:3000/configurations');
+
+            // refresh the page (test the api-key stored in a cookie)
+            cy.visit('/login');
+            cy.url().should('eq', 'http://localhost:3000/configurations');
+
+            cy.get('button')
+                .contains('Logout')
+                .should('have.length', 1)
+                .parents('button')
+                .should('not.be.disabled')
+                .click({force: true});
+
+            cy.url().should('eq', 'http://localhost:3000/login');
+            cy.get('input').should('have.length', 2);
+            cy.get('input').first().should('have.value', '');
+            cy.get('input').last().should('have.value', '');
+            cy.get('button')
+                .contains('Login')
+                .parents('button')
+                .should('be.disabled');
+        });
+    });
+
+    it('shows message for invalid credentials', () => {
+        cy.fixture('account.json').then((json) => {
+            const {
+                username: USERNAME,
+                password: PASSWORD,
+                tmpPassword: TMP_PASSWORD,
+            } = json;
+            cy.visit('/login');
+            cy.get('input').should('have.length', 2);
+            cy.get('input').first().type(USERNAME);
+            cy.get('input').last().type(TMP_PASSWORD);
+
+            cy.get('button')
+                .contains('Login')
+                .parents('button')
+                .should('not.be.disabled')
+                .click();
+
+            cy.url().should('eq', 'http://localhost:3000/login');
+
+            // message colors will be tested in a component test
+            cy.get('div')
+                .contains('Invalid credentials')
+                .should('have.length', 1);
+
+            // message should disappear after typing again
+            cy.get('input').last().clear().type(PASSWORD);
+            cy.get('div')
+                .contains('Invalid credentials')
+                .should('have.length', 0);
+
+            // login should work after an unsuccessful attempt
+            cy.get('button')
+                .contains('Login')
+                .parents('button')
+                .should('not.be.disabled')
+                .click();
+
+            cy.url().should('eq', 'http://localhost:3000/configurations');
+        });
+    });
+});
