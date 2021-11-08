dd22d7da96f9d4ca820915c35e4e8f2fc2f2f27c
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-11-08 12:06:32 +0100

    Add todos for editor test

---
+++ cypress/integration/editor-pages/editor_page.e2e.spec.ts
@@ -0,0 +1,28 @@
+import * as utilities from '../../support/utilities';
+
+const {login} = utilities;
+
+describe('The Editor Page', () => {
+    // TODO: test appearance
+    // - [ ] heading
+    // - [ ] back-button
+    // - [ ] tabs in settings
+    //
+    // TODO: test publish/visibility logic
+    // - [ ] test rendered buttons
+    // - [ ] test db state
+    // - [ ] test reachability via link
+    // - [ ] test toggle (incl. undo)
+    //
+    // TODO: test settings
+    // - [ ] save not possible in invalid state
+    // - [ ] change survey_name
+    //
+    // TODO: test fields
+    // - [ ] fields from fixture-config are rendered correctly
+    // - [ ] save not possible with any invalid field (one of each type, just modify the title, the rest is tested via components)
+    // - [ ] remove field (incl. undo)
+    // - [ ] add field (incl. undo)
+    //
+    // IDEA: transpile UI into JSON object and do asserts on that JSON object
+});
