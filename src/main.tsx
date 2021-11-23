fc050026a1b480194c078a77bb2a6a6f9db754ce
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-11-23 01:08:51 +0100

    Close #140

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+
 import ReduxStore from './redux-store';
 import PageRouter from './page-router';
 
 import 'typeface-quicksand';
 import '/src/styles/tailwind.css';
 import '/src/styles/fixes.css';
+
+// only use sentry when image was built with docker
+if (import.meta.env.MODE === 'production') {
+    Sentry.init({
+        dsn: `${import.meta.env.VITE_SENTRY_API}`,
+        environment: `${import.meta.env.VITE_ENV}`,
+        release: `${import.meta.env.VITE_COMMIT_SHA}`,
+        sampleRate: 1.0,
+    });
+}
 
 ReactDOM.render(
     <React.StrictMode>
