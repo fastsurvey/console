0269d94fb679d3af74e0e5917d88d3b771edbca0
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-11-23 01:28:37 +0100

    Results tests - add todos

---
+++ cypress/integration/results-pages/results_list_page.e2e.spec.ts
@@ -0,0 +1,7 @@
+describe('The Results List Page', () => {
+    //
+    // TODO: Switch a survey (via backend) from draft to published -> should appear in list and look as expected
+    //
+    // TODO: All panels in list work and look as expected
+    //
+});
---
+++ cypress/integration/results-pages/results_summary_page.e2e.spec.ts
@@ -0,0 +1,14 @@
+describe('The Results Summary Page', () => {
+    //
+    // TODO: seed with sample survey
+    //
+    // TODO: check if all results panels contain the expected numbers
+    // TODO: download JSON + check correctness
+    // TODO (maybe): download CSV + check correctness
+    //
+    // TODO: add submission to survey
+    // TODO: expect values to have changed
+    // TODO: download JSON + check correctness
+    // TODO (maybe): download CSV + check correctness
+    //
+});
