6fb29fd4d0324eb735deb7922f10f6d6278b2d63
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-03 21:22:13 +0100

    Refactor suggested email regexes

--- src/utilities/constants/form-options.ts
+++ src/utilities/constants/form-options.ts
@@ -1,9 +1,4 @@
 export const formOptions = {
-    AUTH_MODE: [
-        {value: 0, label: 'No Authentication'},
-        {value: 1, label: 'Email Verification'},
-    ],
-
     MONTHS: [
         'Jan',
         'Feb',
@@ -18,20 +13,6 @@ export const formOptions = {
         'Nov',
         'Dec',
     ],
-    YEARS: [
-        {value: 2020, label: '2020'},
-        {value: 2021, label: '2021'},
-        {value: 2022, label: '2022'},
-        {value: 2023, label: '2023'},
-        {value: 2024, label: '2024'},
-        {value: 2025, label: '2025'},
-    ],
-    HOURS: [...Array(24).keys()].map((i) => {
-        return {label: i.toString(), value: i};
-    }),
-    MINUTES: [...Array(60).keys()].map((i) => {
-        return {label: i.toString(), value: i};
-    }),
     EMAIL_REGEX: [
         {
             label: 'Any Email',
@@ -40,10 +21,10 @@ export const formOptions = {
             hint: 'Any email address',
         },
         {
-            label: 'Unique @mytum.de Email',
+            label: 'Unique @example.com Email',
             value: 1,
-            regex: '[a-z]{2}[0-9]{2}[a-z]{3}@mytum.de',
-            hint: '<LRZ-signature>@mytum.de (e.g. ab12cde@mytum.de), lowercase',
+            regex: '[a-z0-9]+@example.com',
+            hint: '<username>@example.com (e.g. ab12cde@example.com), lowercase',
         },
     ],
 };
