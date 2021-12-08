fbe4c52d60948a27b61c32136c2363bbed6b3e58
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-08 19:31:06 +0100

    Fix commit sha in cloud run tag

--- scripts/deploy-development.sh
+++ scripts/deploy-development.sh
@@ -5,6 +5,7 @@ yarn build
 docker build --platform linux/amd64 -t gcr.io/fastsurvey-infrastructure/console-dev .
 docker push gcr.io/fastsurvey-infrastructure/console-dev:latest
 
+VITE_COMMIT_SHA="$(git rev-parse --short --verify HEAD)"
 gcloud run deploy console-dev \
     --image=gcr.io/fastsurvey-infrastructure/console-dev:latest \
     --platform managed --tag "commit-$VITE_COMMIT_SHA"
--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -5,6 +5,7 @@ yarn build
 docker build --platform linux/amd64 -t gcr.io/fastsurvey-infrastructure/console .
 docker push gcr.io/fastsurvey-infrastructure/console:latest
 
+VITE_COMMIT_SHA="$(git rev-parse --short --verify HEAD)"
 gcloud run deploy console \
     --image=gcr.io/fastsurvey-infrastructure/console:latest \
     --platform managed --no-traffic --tag "commit-$VITE_COMMIT_SHA"
