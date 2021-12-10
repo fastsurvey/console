dcfacc57307d755ffe425789f768d88ada40b0fd
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-10 12:42:00 +0100

    Possibly fix sidebar issue

--- src/components/navbar/visual-regular-navbar.tsx
+++ src/components/navbar/visual-regular-navbar.tsx
@@ -6,7 +6,7 @@ function VisualRegularNavbar(props: {logOut(): void}) {
         <React.Fragment>
             <div
                 className={
-                    'fixed left-0 top-0 px-2 pt-3 pb-1 h-100vh group ' +
+                    'fixed -left-2 top-0 pl-4 pr-2 pt-3 pb-1 h-100vh group ' +
                     'bg-gray-900 flex-col-left shadow no-selection'
                 }
                 data-cy='navbar'
